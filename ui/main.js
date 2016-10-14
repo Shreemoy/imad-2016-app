ebc812becf18ff0f15f9f614490b5971a8f53c3a
Shreemoy <shreemoyrocksmishra97@gmail.com>
2016-10-14 22:01:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,27 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick= function() {
     
     
+    var request = new XMLHttpRequest();
+     
+     request.onreadystatechange = function() {
+       if(request.readystatechange === XMLHttpRequest.DONE) {
+           
+           if(request.status===200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+                    span.innerHTML= counter.toString();
+           }
+       }  
+         
+     };
     
     
     
     
     
+    request.open('GET' , 'http://shreemoy.imad.hasura-app.io/counter' , true);
+    request.send(null);
     
-    
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
 };
